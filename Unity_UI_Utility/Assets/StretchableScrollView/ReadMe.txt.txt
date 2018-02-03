14086d86f7184b598cb9faa37d4a11b4605ea520
DESKTOP-0D8KPI9\14005 <1400539511@qq.com>
2018-02-03 16:33:42 +0800

    15.2  水波效果

---
+++ Unity_UI_Utility/Assets/StretchableScrollView/ReadMe.txt.txt
@@ -0,0 +1,3 @@
+1.可伸缩的scrollView(排版已排好)
+2.ScrollBar随着Content的长度出现，若Content的长度小于ScrollView,则消失
+//TODO list可以做一个拉下来刷新的操作
---
+++ Unity_UI_Utility/Assets/StretchableScrollView/_Scripts/StretchableScrollView.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StretchableScrollView : MonoBehaviour {
+    public RectTransform viewPort;
+    public Scrollbar verticalScrollBar;
+
+    private VerticalLayoutGroup verLayGro;
+    private RectTransform recTra;
+    private ScrollRect scrRec;
+
+	void Start () {
+        Init();
+        UpdateScrollView();
+	}
+    private void Init()
+    {
+        scrRec = GetComponent<ScrollRect>();
+        recTra = GetComponent<RectTransform>();
+        verLayGro = viewPort.GetComponent<VerticalLayoutGroup>();
+        if (verLayGro == null)
+        {
+            Debug.LogError("此脚本是用来拓展‘VerticalLayoutGroup’，此物体没有‘VerticalLayoutGroup’");
+        }
+        viewPort.pivot = new Vector2(viewPort.pivot.x, 1f); 
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+            UpdateScrollView();
+    }
+
+    private void UpdateScrollView()
+    {
+        #region 根据子物体计算高度
+        float height = 0;
+        int top = verLayGro.padding.top;
+        int bottom = verLayGro.padding.bottom;
+        float spaces = (viewPort.childCount - 1) * verLayGro.spacing;
+
+        float childrenHeight = 0;
+        for (int i = 0; i < viewPort.childCount;i++)
+        {
+            childrenHeight += viewPort.GetChild(0).GetComponent<RectTransform>().rect.height;
+        }
+
+        height = top + bottom + spaces + childrenHeight;
+        viewPort.sizeDelta = new Vector2(viewPort.sizeDelta.x,height);
+        #endregion
+
+        //若高度大于父物体的高度，显示scrollbar
+        if (height < recTra.rect.height)
+        {
+            scrRec.verticalScrollbar = null;
+            verticalScrollBar.gameObject.SetActive(false);
+        }
+        else
+        {
+            scrRec.verticalScrollbar = verticalScrollBar;
+            verticalScrollBar.gameObject.SetActive(true);
+        }
+
+
+    }
+}
