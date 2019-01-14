75433caa95dbbf2092db1b18bd665c20f3691e30
Potro <1400539511@qq.com>
2019-01-14 11:16:32 +0800

    添加一个confirmwindow

---
+++ Unity_Utility/Assets/Editor/TipWindows/ConfirmWindow.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+public class ConfirmWindow : EditorWindow
+{
+    string tipText = "Are you sure ?";
+    Action action = null;
+
+    static void Create(string tipText,Action action)
+    {
+        ConfirmWindow window = (ConfirmWindow)EditorWindow.GetWindow(typeof(ConfirmWindow));
+        window.tipText = tipText;
+        window.action = action;
+        window.Show();
+    }
+
+    static void Create(Action action)
+    {
+        ConfirmWindow window = (ConfirmWindow)EditorWindow.GetWindow(typeof(ConfirmWindow));
+        window.action = action;
+        window.Show();
+    }
+
+    [MenuItem("Chen/Test")]
+    static void Test()
+    {
+        ConfirmWindow.Create("你确定要执行这个操作嘛？",()=> { Debug.Log("执行操作"); });
+    }
+
+    void OnGUI()
+    {
+        EditorGUILayout.Space();
+
+        GUILayout.Label(tipText, EditorStyles.boldLabel);
+
+        EditorGUILayout.Space();
+
+        EditorGUILayout.BeginHorizontal();
+        if (GUILayout.Button("OK"))
+        {
+            this.Close();
+            action();
+        }
+        if (GUILayout.Button("Cancel"))
+            this.Close();
+        EditorGUILayout.EndHorizontal();
+    }
+}
