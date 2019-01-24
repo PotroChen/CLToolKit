0f703b6d8f67093ecf0dba9f72d28b77e6b181a7
Potro <1400539511@qq.com>
2019-01-24 11:07:44 +0800

    删除

--- Unity_Utility/Assets/0.MyResKit/Scripts/Info/AssetBundleInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-
-[Serializable]
-public class AssetBundleInfo {
-
-    public List<AssetInfo> AssetInfos = new List<AssetInfo>();
-    public string AssetBundleName;
-}
--- Unity_Utility/Assets/0.MyResKit/Scripts/Info/AssetInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-
-[Serializable]
-public class AssetInfo {
-    public string AssetName;
-    public string OwnerAssetBundleName;
-}
