a281f9148eea50951c6bf2912427398135dfa1fa
DESKTOP-0D8KPI9\14005 <1400539511@qq.com>
2019-11-24 20:25:39 +0800

    添加AppRoot

---
+++ CLToolKit/Assets/GameBasic/AppRoot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppRoot : MonoBehaviour {
+
+	public static ResLoader GlobalResLoader = new ResLoader();
+	void Awake()
+	{
+		DontDestroyOnLoad(gameObject);	
+		ResMgr.Init();	
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
